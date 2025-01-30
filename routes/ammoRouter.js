fab4e9a3692b78de160d4edd5f14c7b8bf222477
Jason Finkelstein <jason@finkmail.net>
2025-01-30 14:35:47 -0500

    Updates route for the home page

--- routes/ammoRouter.js
+++ routes/ammoRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.render('home', {data:[], searchQuery: ''});
+    res.render('ammo', {data:[], searchQuery: ''});
 });
 
 
